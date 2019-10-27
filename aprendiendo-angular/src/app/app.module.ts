a0f7781eb7919052050dde8d099ab22237161113
SelvinMedina <selvinmedina0016@gmail.com>
2019-10-27 00:34:02 -0600

    Routing y navegacion

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MiComponente } from './components/mi-componente/mi-componente.component';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { PeliculasComponent } from './peliculas/peliculas.component';
@@ -8,6 +9,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +23,18 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent,
     SliderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    PaginaComponent,
+    FormularioComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,27 @@
+// Importar los modulos del router de angular
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Importar los componentes a los cuales les quiero hacer una pagina exclusiva
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { ErrorComponent } from './components/error/error.component';
+
+// Array de rutas
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'form', component: FormularioComponent },
+    { path: 'peliculas', component: PeliculasComponent },
+    { path: 'pagina-de-pruebas', component: PaginaComponent },
+    { path: 'pagina-de-pruebas/:nombre/:apellidos', component: PaginaComponent },
+    { path: '**', component: ErrorComponent }
+];
+
+// Exportar el modulo de rutas
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
