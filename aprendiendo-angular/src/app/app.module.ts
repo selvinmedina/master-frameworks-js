698dd9c322ab11434496930ed294a60156e2db9d
SelvinMedina <selvinmedina0016@gmail.com>
2019-10-26 23:34:38 -0600

    Agregar los componentes footer, sidebar y header

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { MiComponente } from './components/mi-componente/mi-componente.component
 
 import { AppComponent } from './app.component';
 import { PeliculasComponent } from './peliculas/peliculas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiComponente,
-    PeliculasComponent
+    PeliculasComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
