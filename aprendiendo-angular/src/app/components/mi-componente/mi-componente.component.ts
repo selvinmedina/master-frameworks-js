81996f4ca2740769288ae87c2b9bace5f2c09d72
SelvinMedina <selvinmedina0016@gmail.com>
2019-10-26 22:38:28 -0600

    Hooks hechos

--- aprendiendo-angular/src/app/components/mi-componente/mi-componente.component.ts
+++ aprendiendo-angular/src/app/components/mi-componente/mi-componente.component.ts
@@ -9,16 +9,21 @@ export class MiComponente {
     public titulo: string;
     public comentario: string;
     public year: number;
+    public mostrarPelicula: boolean;
 
     constructor() {
         console.log('Componente mi-componente cargado!');
         this.titulo = 'Hola mundo desde mi componente';
         this.comentario = 'Holaa';
+        this.mostrarPelicula = true;
         this.year = 2020;
 
         console.log(this.titulo);
         console.log(this.comentario);
         console.log(this.year);
-        
+    }
+
+    ocultarPelicula() {
+        this.mostrarPelicula = false;
     }
 }
--- aprendiendo-angular/src/app/peliculas/peliculas.component.ts
+++ aprendiendo-angular/src/app/peliculas/peliculas.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'peliculas',
   templateUrl: './peliculas.component.html',
   styleUrls: ['./peliculas.component.css']
 })
-export class PeliculasComponent implements OnInit {
+export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
 
-  constructor() { }
+  public titulo: string;
 
-  ngOnInit() {
+  constructor() {
+    this.titulo = 'Componente de peliculas';
+    console.log('Constructor lanzado');
   }
 
+  ngOnInit() {
+    console.log('Componente iniciado');
+  }
+
+  ngDoCheck() {
+    console.log('doCheck lanzado');
+  }
+
+  cambiarTitulo() {
+    this.titulo = 'El titulo ha sido cambiado';
+  }
+
+  ngOnDestroy(): void {
+    console.log('El componente se va a eliminar');
+  }
 }
