dd9fdd05994858c8019bc95e71ad2345fe87751b
SelvinMedina <selvinmedina0016@gmail.com>
2019-10-28 19:39:37 -0600

    Guardar articulos

--- aprendiendo-angular/src/app/components/article-new/article-new.component.ts
+++ aprendiendo-angular/src/app/components/article-new/article-new.component.ts
@@ -1,24 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../../models/article';
+import { ArticleService } from '../../services/article.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-article-new',
   templateUrl: './article-new.component.html',
-  styleUrls: ['./article-new.component.css']
+  styleUrls: ['./article-new.component.css'],
+  providers: [ArticleService]
 })
 export class ArticleNewComponent implements OnInit {
 
   public article: Article;
+  public status: string;
 
-  constructor() {
-    this.article = new Article('','','',null,null);
-   }
+  constructor(
+    private _articleService: ArticleService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {
+    this.article = new Article('', '', '', null, null);
+  }
 
   ngOnInit() {
   }
 
-  onSubmit(){
-    console.log(this.article);
+  onSubmit() {
+    this._articleService.create(this.article).subscribe(
+      response => {
+        this.status = 'success'
+        if (response.status == 'success') {
+          this.status = 'success';
+          this.article = response.article;
+          this._router.navigate(['/blog']);
+        } else {
+          this.status = 'error';
+        }
+      },
+      err => {
+        console.log(err);
+        this.status = 'error';
+      }
+    )
   }
 
 }
