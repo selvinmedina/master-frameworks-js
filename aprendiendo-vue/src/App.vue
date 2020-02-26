7428e8d38ea437ffa6c7c5371f697d37512ffbfc
selvinmedina <selvinmedina0016@gmail.com>
2020-02-25 23:03:05 -0600

    styles vue

--- aprendiendo-vue/src/App.vue
+++ aprendiendo-vue/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hola mundo"/>
+    <MiComponente/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import MiComponente from './components/MiComponente.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MiComponente
   }
 }
 </script>
---
+++ aprendiendo-vue/src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="miComponente">
+        <hr>
+        <h1>Este es mi componente</h1>
+        <h2>{{subtitulo}}</h2>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+    data(){
+        return {
+            subtitulo: 'Soy el subtitulo de mi componente'
+        }
+    }
+}
+</script>
